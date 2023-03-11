97bc1dc172685bee6d6fd17d5cd047ca0bd82f1e
Waldemar Kubiak <kubiak.waldemar@gmail.com>
2023-03-11 13:15:16 +0100

    Task-01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+stopBtn.disabled = true;
+let timerId = null;
+
+function startChangeColor() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function stopChangeColor() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+startBtn.addEventListener('click', startChangeColor);
+stopBtn.addEventListener('click', stopChangeColor);
+
+// console.log(body);
+// console.log(startBtn);
+// console.log(stopBtn);
